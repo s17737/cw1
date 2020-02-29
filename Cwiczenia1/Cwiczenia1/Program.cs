63d1e86b825d53aa358753c82ba92c475f536546
s17737 <s17737@pjwstk.edu.pl>
2020-02-29 19:38:19 +0100

    wyszukiwarka maili na stronie pjwstk

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cwiczenia1
+{
+    class Program
+    {
+        static async Task Main(string[] args) //dodalismy async, bo to dla wątku
+        {
+            var client = new HttpClient(); //do wykonywania zadan http. 
+            var result = await client.GetAsync("https://www.pja.edu.pl"); //odpalane na nowym wątku, zeby nie blokowac
+
+            if(result.IsSuccessStatusCode) //dowolna wartosc z kodem 200+
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",
+                    RegexOptions.IgnoreCase);
+                var matches = regex.Matches(html);
+
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
+            }
+        }
+    }
+}
